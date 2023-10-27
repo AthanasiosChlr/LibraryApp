edf62a18c1fb19475131d0e2fc74e130768b1d89
SakisXlr <sakismx@yahoo.com>
2023-10-27 14:43:16 +0300

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import './CSS/index.css'
 import Header from './Header'
 import Form from './Form'
-import background from "../images/background.jpg"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
  <React.StrictMode>
